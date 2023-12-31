c3bd03c902c0319e63905d27c71e3c56b1968c25
rebecaslvs <rebecaalves145@gmail.com>
2023-12-31 18:29:09 -0300

    atualização final do código e correção de bug

--- js/main.js
+++ js/main.js
@@ -1,53 +1,55 @@
 $(document).ready(function () {
   $("#carrossel-imagens").slick({ autoplay: true });
-});
 
-$(".menu-hamburguer").click(function () {
-  $("nav").slideToggle(); //slideToggle é uma função do jQuery que faz o menu aparecer e desaparecer sem que a gente precise ficar verificando
-});
+  $(".menu-hamburguer").click(function () {
+    $("nav").slideToggle(); //slideToggle é uma função do jQuery que faz o menu aparecer e desaparecer sem que a gente precise ficar verificando
+  });
 
-$("#telefone").mask("(00) 00000-0000", {
-  placeholder: "(__) _____-____",
-});
+  $("#telefone").mask("(00) 00000-0000");
 
-$("form").validate({
-  rules: {
-    nome: {
-      required: true,
+  $("form").validate({
+    rules: {
+      nome: {
+        required: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      telefone: {
+        required: true,
+      },
+      mensagem: {
+        required: true,
+      },
+      veiculoDeInteresse: {
+        required: false,
+      },
     },
-    email: {
-      required: true,
-      email: true,
+    messages: {
+      nome: "Por favor, informe seu nome",
+      email: "Por favor, informe um e-mail válido",
+      telefone: "Por favor, informe o seu telefone",
+      mensagem: "Por favor, informe uma mensagem",
     },
-    telefone: {
-      required: true,
+    submitHandler: function (form) {
+      console.log(form);
     },
-    mensagem: {
-      required: true,
+    invalidHandler: function (evento, validador) {
+      let camposIncorretos = validador.numberOfInvalids();
+      if (camposIncorretos) {
+        alert(`Existem ${camposIncorretos} campos incorretos`);
+      }
     },
-    veiculoDeInteresse: {
-      required: false,
-    },
-  },
-  messages: {
-    nome: "Por favor, informe seu nome",
-    email: "Por favor, informe um e-mail válido",
-    telefone: "Por favor, informe o seu telefone",
-    mensagem: "Por favor, informe uma mensagem",
-  },
-  submitHandler: function (form) {
-    console.log(form);
-  },
-  invalidHandler: function (evento, validador) {
-    let camposIncorretos = validador.numberOfInvalids();
-    if (camposIncorretos) {
-      alert(`Existem ${camposIncorretos} campos incorretos`);
-    }
-  },
+  });
 });
 
 $(".lista-veiculos button").click(function () {
   const destino = $("#contato");
+
+  const nomeVeiculo = $(this).parent().find("h3").text();
+
+  $("#veiculoDeInteresse").val(nomeVeiculo);
 
   $("html").animate(
     {
